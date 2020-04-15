b9891796dbba50a518aee4566642cb1d6c643eff
Karthik <chejerlakarthik@github.com>
2020-04-15 16:26:35 +1000

    Added jacoco and sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.2.2.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	id("org.sonarqube") version "2.8"
+	id("jacoco")
+	id("java")
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
 }
@@ -37,6 +40,62 @@ dependencies {
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.3")
 	testImplementation("io.kotlintest:kotlintest-extensions-spring:3.3.3")
+}
+
+sonarqube {
+	properties {
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.projectName", "user-service")
+		property("sonar.projectKey", "chejerlakarthik_user-service")
+		property("sonar.projectVersion", "1.0")
+		property("sonar.junit.reportPaths", "user-service/build/test-results/test")
+		property("sonar.jacoco.reportPaths", "build/jacoco/test.exec")
+	}
+}
+
+jacoco {
+	toolVersion = "0.8.5"
+	reportsDir = file("$buildDir/jacocoReports")
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.isEnabled = false
+		csv.isEnabled = false
+		html.destination = file("${buildDir}/jacocoHtml")
+	}
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.check {
+	dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			limit {
+				counter = "LINE"
+				value = "COVEREDRATIO"
+				minimum = "0.1".toBigDecimal()
+			}
+		}
+
+//		rule {
+//			enabled = false
+//			element = "CLASS"
+//			includes = listOf("org.gradle.*")
+//
+//			limit {
+//				counter = "LINE"
+//				value = "TOTALCOUNT"
+//				maximum = "0.3".toBigDecimal()
+//			}
+//		}
+	}
 }
 
 tasks.withType<Test> {
